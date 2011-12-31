f7e6ba819debb55629d98acc11434605f8d01ced
cuzic <shopping@cuzic.com>
2012-01-01 02:15:39 +0900

    blogos_parser fixed for Blogos site renewal

--- plugins/blogos/blogos_parser.rb
+++ plugins/blogos/blogos_parser.rb
@@ -1,5 +1,9 @@
 # coding: utf-8
-require 'lib/parser'
+
+if not defined? TAKIBI_ROOT then
+  TAKIBI_ROOT = File.expand_path(File.join(File.dirname(__FILE__),"..",".."))
+end
+require File.join(TAKIBI_ROOT,'lib/parser')
 
 module Takibi
   class BlogosParser < Parser
@@ -11,24 +15,26 @@ class Takibi::BlogosParser
     super src, url
   end
 
-  rss_regex %r(news.livedoor.com/article/detail)
+  rss_regex %r(blogos.com/article/)
 
   #title_xpath          '//div[@class="article-title" or @id="article-title"]/h1/text()'
-  title_xpath          '//meta[@name="title"]/@content | //div[@id="article-title"]/h1'
-  published_time_xpath '//p[@class="article-date"]/text()'
-  author_xpath         '//p[@class="author"]/text()'
+  title_xpath          '//meta[@name="title"]/@content | //div[@class="blogos_article_title"]/h1'
+  published_time_xpath '//div[@class="blogos_article_title"]//p[@class="update"]'
+  author_xpath         '//div[@class="blogos_article_title"]//p[@class="author-name24"]'
   images_xpath         ''
   image_caption_xpath  ''
 
-  body_xpath           '//div[@class="article"]'
+  body_xpath           '//div[@class="blogos_article"]'
   noisy_elems_xpaths   %W()
-  next_link_xpath      '//td[@class="link-next"]/a'
+  next_link_xpath      '//div[@class="pager"]/a[@class="next"]'
 end
 
 if $0 == __FILE__ then
   require 'open-uri'
-  url = "http://news.livedoor.com/article/detail/5575137/"
+  url = "http://blogos.com/article/28188/"
+  url = "http://blogos.com/article/28113/"
   src = open(url).read
   parsed = Takibi::BlogosParser.extract src, url
+  puts parsed["next_link"]
   puts parsed["title"]
 end
